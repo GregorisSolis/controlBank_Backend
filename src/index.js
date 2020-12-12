20575bde33f7bf615f5f42fa80927e0520b2b773
Gregoris solis <solisgregorip@gmail.com>
2020-12-12 15:47:50 -0200

    AGREGUE CORS AL SERVIDOR, TAMBIEN AGREGUE LA PUERTA PARA PODER USAR LA API EN HEROKU

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-var express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser')
-var app = express();
 
+const cors = require('cors')
+const app = express();
+
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 require('./app/controllers/index')(app)
 
-app.listen(3000, function() {
-  console.log('escutando na porta 3000!');
-});
+app.listen(process.env.PORT || 3000)
+console.log('Servidor Activo OK')
