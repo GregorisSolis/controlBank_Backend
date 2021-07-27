374c1efb977907373c9811722e4b089f6b79bbd4
Gregoris solis <solisgregorip@gmail.com>
2021-07-27 17:20:52 -0300

    agregue el schema y la ruta para agregar las fotos, problema: se guarda aun en el disco

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ const app = express();
 app.use(cors())
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 require('./app/controllers/index')(app)
+
+app.use(require('./routes'));
 
 app.listen(process.env.PORT || 3030)
 console.log('Servidor Activo OK')
